33e1f6505fcfd164553d57b06bcc5dc4c686cc34
devcastro9 <castro.q.orlando@gmail.com>
2026-04-05 18:02:25 -0400

    carga

--- obt_prevision_rep.sql
+++ obt_prevision_rep.sql
@@ -1,135 +1,98 @@
-PROCEDURE pr.obt_prevision_rep (
-    p_cod_empresa  IN  VARCHAR2,
-    pr_cod_cliente IN  VARCHAR2,
-    p_fecha        IN  DATE,
-    tot_prevision  OUT NUMBER,
-    tot_pre_con    OUT NUMBER,
-    p_error OUT VARCHAR2
-)
+FUNCTION pr.obt_prevision_rep (
+    p_cod_empresa  IN VARCHAR2,
+    p_cod_cliente IN VARCHAR2,
+    p_fecha        IN DATE
+) RETURN SYS_REFCURSOR
 IS
     --
-    -- Obtiene los tramites del Cliente, con sus respectivas previsiones
+    -- Obtiene los tramites del Cliente, con sus respectivas previsiones.
+    -- Retorna un REF CURSOR con tot_prevision y tot_pre_con.
     --
 
-    CURSOR cur_tramite IS
-        SELECT PR_TRAMITE.num_tramite,
-               PR_TRAMITE.codigo_estado,
-               PR_TRAMITE.cod_moneda,
-               PR_TRAMITE.bajo_linea_credito,
-               PR_TIP_CREDITO.descripcion,
-               PR_TRAMITE.cod_tip_operacion,
-               PR_TRAMITE.unidad_ejecutora
-          FROM pr_tramite,
-               personas_x_pr_tramite a,
-               pr_tip_credito,
-               pr_tip_operacion
-         WHERE PR_TRAMITE.cod_tip_operacion = PR_TIP_OPERACION.cod_tip_operacion
-           AND PR_TRAMITE.cod_tip_operacion IN (
-                   pa.PARAMETRO_GENERAL('PR', 'COD_OPER_CARTERA'),
-                  pa.PARAMETRO_GENERAL('PR', 'COD_OPER_COMEX'),
-                   pa.PARAMETRO_GENERAL('PR', 'COD_OPER_SOBREGIRO'),
-                   pa.PARAMETRO_GENERAL('PR', 'COD_OPER_TARJETA'),
-                   pa.PARAMETRO_GENERAL('PR', 'COD_OPER_FACTORAJE')
-               )
-           AND PR_TRAMITE.codigo_estado     = Pr_Utl_Estados.Verif_Estado_Act_Cast(Pr_Tramite.Codigo_Estado)
-           AND PR_TIP_CREDITO.cod_tip_credito = PR_TRAMITE.cod_tip_credito
-           AND PR_TIP_CREDITO.cod_empresa    = PR_TRAMITE.cod_empresa
-           AND PR_TRAMITE.cod_empresa        = p_cod_empresa
-           AND a.cod_empresa                 = pr_tramite.cod_empresa
-           AND a.num_tramite                 = pr_tramite.num_tramite
-           AND a.cod_persona                 = pr_cod_cliente;
+    -- Codigos de operacion cacheados (evita releer PA.PARAM_GENERALES por cada fila del cursor)
+    v_op_cartera     PA.PARAM_GENERALES.ABREV_PARAMETRO%TYPE;
+    v_op_comex       PA.PARAM_GENERALES.ABREV_PARAMETRO%TYPE;
+    v_op_sobregiro   PA.PARAM_GENERALES.ABREV_PARAMETRO%TYPE;
+    v_op_tarjeta     PA.PARAM_GENERALES.ABREV_PARAMETRO%TYPE;
+    v_op_factoraje   PA.PARAM_GENERALES.ABREV_PARAMETRO%TYPE;
+    v_cod_moneda_usd PA.PARAM_GENERALES.ABREV_PARAMETRO%TYPE;
 
-    -- Variables de Tarjetas de Créditos
-    p_nro_tarjeta  NUMBER;
-    p_fec_habilit  DATE;
-    p_fec_vencimi  DATE;
-    p_monto_aprob  NUMBER;
-    p_saldo_actua  NUMBER;
-    p_mensaje      VARCHAR2(20);
+    TYPE t_cur_tramite IS REF CURSOR;
+    cur_tramite t_cur_tramite;
 
-    -- Variables de Sobregiros
-    Pcta_efectivo      NUMBER;
-    pFec_Inicio        DATE;
-    pCan_Dias_Gracia   NUMBER;
-    pTasa_Interes      NUMBER;
-    pFec_Vencimiento   DATE;
-    pMon_Contratado    NUMBER;
-    pPlazo_Dias        NUMBER;
-    pFec_Cancelacion   DATE;
-    pDias_Atraso       NUMBER;
-    pFec_Ult_Mov       DATE;
-    pSaldo_Vig         NUMBER;
-    pCta_Vig           VARCHAR2(20);
-    pInteres_Vig       NUMBER;
-    pCta_Int_Vig       VARCHAR2(20);
-    pSaldo_Ven         NUMBER;
-    pCta_Ven           VARCHAR2(20);
-    pInteres_Ven       NUMBER;
-    pCta_Int_Ven       VARCHAR2(20);
-    pInt_Susp_Ven      NUMBER;
-    pCta_Int_Susp_Ven  VARCHAR2(20);
-    pSaldo_Eje         NUMBER;
-    pCta_Eje           VARCHAR2(20);
-    pInteres_Eje       NUMBER;
-    pCta_Int_Eje       VARCHAR2(20);
-    pCod_Error         VARCHAR2(20);
-    pCta_Contingente   VARCHAR2(20);
-    vl_mon_contingente NUMBER(16,2);
-    vl_fec_estado      DATE;
-    pNumOperacion      NUMBER;
-    pFechaInicio       DATE;
-    pTasaInteres       NUMBER;
-    pFechaVence        DATE;
-    pMonContratado     NUMBER;
-    pPlazoDias         NUMBER;
-    pFechaCance        DATE;
-    pDiasAtraso        NUMBER;
-    pFecUltMov         DATE;
-    pSaldoVig          NUMBER;
-    pCtaSaldoVig       VARCHAR2(20);
-    pInteresVig        NUMBER;
-    pCtaIntVig         VARCHAR2(20);
-    pSaldoVen          NUMBER;
-    pCtaSaldoVen       VARCHAR2(20);
-    pInteresVen        NUMBER;
-    pCtaIntVen         VARCHAR2(20);
-    pSaldoEjec         NUMBER;
-    pCtaSaldoEjec      VARCHAR2(20);
-    pMsjError          VARCHAR2(20);
-    v_num_proceso      pr_provisiones.num_proceso%TYPE;
-    v_pre_con          NUMBER(16,2);
-    v_prevision        NUMBER(16,2);
-    v_cod_moneda_usd    PA.PARAM_GENERALES.ABREV_PARAMETRO%TYPE;
+    TYPE t_rec_tramite IS RECORD (
+        num_tramite        PR_TRAMITE.num_tramite%TYPE,
+        codigo_estado      PR_TRAMITE.codigo_estado%TYPE,
+        cod_moneda         PR_TRAMITE.cod_moneda%TYPE,
+        bajo_linea_credito PR_TRAMITE.bajo_linea_credito%TYPE,
+        descripcion        PR_TIP_CREDITO.descripcion%TYPE,
+        cod_tip_operacion  PR_TRAMITE.cod_tip_operacion%TYPE,
+        unidad_ejecutora   PR_TRAMITE.unidad_ejecutora%TYPE
+    );
+    reg_tramite t_rec_tramite;
+
+    v_num_proceso   PR_PROVISIONES.num_proceso%TYPE;
+    v_pre_con       NUMBER(16,2);
+    v_prevision     NUMBER(16,2);
+    v_tot_pre_con   NUMBER := 0;
+    v_tot_prevision NUMBER := 0;
+    v_error         VARCHAR2(200);
+    v_rc            SYS_REFCURSOR;
 BEGIN
-    --
-    -- Obtiene nro. de proceso de prevision
-    --
-   v_cod_moneda_usd := pa.PARAMETRO_GENERAL('PR', 'COD_MONEDA_DOLAR');-- [25/03/2026]
-    BEGIN
-        SELECT MAX(num_proceso)
-          INTO v_num_proceso
-          FROM PR_PROVISIONES
-         WHERE cod_empresa                    = p_cod_empresa
-           AND TRUNC(fec_ult_calificacion)   <= p_fecha
-           AND provisionado                   = 'S';
-    END;
+    -- Cachear parametros una sola vez
+    v_cod_moneda_usd := pa.PARAMETRO_GENERAL('PR', 'COD_MONEDA_DOLAR');  -- [25/03/2026]
+    v_op_cartera     := pa.PARAMETRO_GENERAL('PR', 'COD_OPER_CARTERA');
+    v_op_comex       := pa.PARAMETRO_GENERAL('PR', 'COD_OPER_COMEX');
+    v_op_sobregiro   := pa.PARAMETRO_GENERAL('PR', 'COD_OPER_SOBREGIRO');
+    v_op_tarjeta     := pa.PARAMETRO_GENERAL('PR', 'COD_OPER_TARJETA');
+    v_op_factoraje   := pa.PARAMETRO_GENERAL('PR', 'COD_OPER_FACTORAJE');
 
-    FOR reg_tramite IN cur_tramite LOOP
+    -- Obtiene nro. de proceso de prevision vigente
+    SELECT MAX(num_proceso)
+      INTO v_num_proceso
+      FROM PR_PROVISIONES
+     WHERE cod_empresa                  = p_cod_empresa
+       AND TRUNC(fec_ult_calificacion) <= p_fecha
+       AND provisionado                 = 'S';
 
-        --:bkdetpre.num_tramite       := reg_tramite.num_tramite;
-        --:bkdetpre.codigo_estado     := reg_tramite.codigo_estado;
-        --:bkdetpre.cod_moneda        := reg_tramite.cod_moneda;
-        --:bkdetpre.cod_tip_operacion := reg_tramite.cod_tip_operacion;
-        --:bkdetpre.tipo_operacion    := reg_tramite.descripcion;
-        --:bkdetpre.linea             := reg_tramite.bajo_linea_credito;
+    -- Cursor con variables locales ya cacheadas (ANSI JOIN)
+    OPEN cur_tramite FOR
+        SELECT PT.num_tramite,
+               PT.codigo_estado,
+               PT.cod_moneda,
+               PT.bajo_linea_credito,
+               PTC.descripcion,
+               PT.cod_tip_operacion,
+               PT.unidad_ejecutora
+          FROM PR_TRAMITE PT
+               JOIN personas_x_pr_tramite A
+                   ON  A.cod_empresa = PT.cod_empresa
+                   AND A.num_tramite = PT.num_tramite
+               JOIN PR_TIP_CREDITO PTC
+                   ON  PTC.cod_tip_credito = PT.cod_tip_credito
+                   AND PTC.cod_empresa     = PT.cod_empresa
+               JOIN PR_TIP_OPERACION PTO
+                   ON  PTO.cod_tip_operacion = PT.cod_tip_operacion
+         WHERE PT.cod_tip_operacion IN (
+                   v_op_cartera, v_op_comex, v_op_sobregiro,
+                   v_op_tarjeta, v_op_factoraje
+               )
+           AND PT.codigo_estado = Pr_Utl_Estados.Verif_Estado_Act_Cast(PT.Codigo_Estado)
+           AND PT.cod_empresa   = p_cod_empresa
+           AND A.cod_persona    = p_cod_cliente;
 
-        ---
-        --- Obtiene prevision
-        ---
+    LOOP
+        FETCH cur_tramite INTO reg_tramite;
+        EXIT WHEN cur_tramite%NOTFOUND;
+
+        -- Resetear por iteracion: evita acumular valores de iteraciones anteriores
+        -- cuando este tramite no tiene fila en PR_HIS_CALIF_X_PR_TRAMITE
+        v_pre_con   := 0;
+        v_prevision := 0;
+
         BEGIN
-            -- SELECT mon_pre_contin, mon_prevision
             SELECT mon_pre_contin,
-                   mon_prevision + NVL(mon_prev_diferido, 0)  -- OBCHOQUE 13/06/2022 MA 373820, Se adiciona la previsión diferida.
+                   mon_prevision + NVL(mon_prev_diferido, 0)  -- OBCHOQUE 13/06/2022 MA 373820, adiciona prevision diferida
               INTO v_pre_con,
                    v_prevision
               FROM PR_HIS_CALIF_X_PR_TRAMITE
@@ -137,42 +100,42 @@ BEGIN
                AND num_tramite = reg_tramite.num_tramite
                AND num_proceso = v_num_proceso;
         EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                NULL;
+            WHEN NO_DATA_FOUND THEN NULL;
         END;
 
-        IF reg_tramite.cod_moneda <>     v_cod_moneda_usd THEN
-            --
-            -- Cambia la moneda a Dolares Prevision Contingente
-            --
+        IF reg_tramite.cod_moneda <> v_cod_moneda_usd THEN
+            -- Convierte prevision contingente a USD
             Convierte_Moneda_a_Moneda(
                 p_cod_empresa,
                 v_pre_con,
                 p_fecha,
                 TO_NUMBER(reg_tramite.cod_moneda),
                 v_cod_moneda_usd,
-                p_error,
+                v_error,
                 v_pre_con
             );
-
-            --
-            -- Cambia la moneda a Dolares Prevision Normal
-            --
+            -- Convierte prevision normal a USD
             Convierte_Moneda_a_Moneda(
                 p_cod_empresa,
                 v_prevision,
                 p_fecha,
                 TO_NUMBER(reg_tramite.cod_moneda),
                 v_cod_moneda_usd,
-                p_error,
+                v_error,
                 v_prevision
             );
-
         END IF;
 
-        tot_pre_con   := NVL(tot_pre_con,   0) + NVL(v_pre_con,   0);
-        tot_prevision := NVL(tot_prevision, 0) + NVL(v_prevision, 0);
+        v_tot_pre_con   := v_tot_pre_con   + NVL(v_pre_con,   0);
+        v_tot_prevision := v_tot_prevision + NVL(v_prevision, 0);
 
     END LOOP;
+    CLOSE cur_tramite;
+
+    OPEN v_rc FOR
+        SELECT v_tot_prevision AS tot_prevision,
+               v_tot_pre_con  AS tot_pre_con
+          FROM DUAL;
+    RETURN v_rc;
 
 END;
